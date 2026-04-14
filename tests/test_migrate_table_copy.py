b76cc098cd9e1417fc52742bd9419ea0a193caae
Mikke Schirén <mikke.schiren@digitalist.com>
2026-04-14 08:05:45 +0200

    update test to match change

--- tests/test_migrate_table_copy.py
+++ tests/test_migrate_table_copy.py
@@ -53,7 +53,7 @@ def test_migrate_table_copy_full_path(monkeypatch):
 
     # COPY SQL
     copy_sql = pg_cursor.copy_expert.call_args.args[0]
-    assert copy_sql == "COPY test (id, payload) FROM STDIN WITH CSV"
+    assert copy_sql == "COPY test (id, payload) FROM STDIN WITH CSV NULL '__NULL__'"
 
     # Verify streamed CSV content
     streamed_data = "".join(captured_data)
